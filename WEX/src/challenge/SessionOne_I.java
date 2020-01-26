85ba370edada041cce10ed60da2f0a618fe09e87
Bruno Araújo <50248497+hjamesp@users.noreply.github.com>
2020-01-26 20:37:52 -0300

    Update SessionOne_I.java

--- WEX/src/challenge/SessionOne_I.java
+++ WEX/src/challenge/SessionOne_I.java
@@ -1,3 +1,38 @@
+/*Feature: Numbers divisible by 3 that contains 5 in it
+ * As a user I want the program to print "Cira" when a number is divisible by 3
+ * And print "Dinha" when the number contains 5 in it
+ * And print "CiraDinha" when a number is divisible by 3 and contains 5 in it
+ * 
+ * Scenario: Divisible by 3
+ *	 	Given given that I enter a number
+ * 		When that program ask for a number
+ * 		Then then I type a number
+ * 		And I press Ok
+ * 		When the number is divisible by 3 
+ * 		Then program will print "Cira".
+ * Scenario: The number contain 5 in it
+ * 		Given given that I enter a number
+ * 		When that program ask for a number
+ * 		Then then I type a number
+ * 		And I press Ok
+ * 		When the number contains 5 in it 
+ * 		Then program will print "Dinha".
+ * Scenario: The number is divisible by 3 and contain 5 in it
+ * 		Given given that I enter a number
+ * 		When that program ask for a number
+ * 		Then then I type a number
+ * 		And I press Ok
+ * 		When the number is divisible by 3 and contains 5 in it 
+ * 		Then program will print "CiraDinha".
+ * Scenario: The number isn't divisible by 3 and don't contain 5 in it
+ * 		Given given that I enter a number
+ * 		When that program ask for a number
+ * 		Then then I type a number
+ * 		And I press Ok
+ * 		When the number isn't divisible by 3 and don't contains 5 in it 
+ * 		Then program will print "The number typed it's not divisible by 3 and don't contains 5 in it.".
+ */
+
 package challenge;
 
 import javax.swing.JOptionPane;
@@ -7,10 +42,6 @@ public class SessionOne_I {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    	
-        //#1 - Write a program that will print “Cira” for numbers that are divisible by 3, 
-    	//print “Dinha” for number that contains “5” in it and print “CiraDinha” when the 
-    	//number is divisible by 3 and contains 5 in it.
     	
         Long number;
         String parameter;
