47b2ef1ed158c5d2dbdc743025193774d559e9eb
Bruno Araújo <50248497+hjamesp@users.noreply.github.com>
2020-01-26 20:38:21 -0300

    Update SessionOne_II.java

--- WEX/src/challenge/SessionOne_II.java
+++ WEX/src/challenge/SessionOne_II.java
@@ -1,3 +1,14 @@
+/*Feature: Sum of all digits typed
+ * As a user I want the program to sum all digits of a integer number
+ * 
+ * Scenario: Summing
+ *	 	Given given that I enter a number
+ * 		When that program ask for a number
+ * 		Then then I type a number
+ * 		And I press Ok
+ * 		Then program will print the result of the sum aff all digits.
+*/
+
 package challenge;
 
 import javax.swing.JOptionPane;
